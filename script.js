b8b058a6b33bf7676e47125d2076d770a8f219ac
linas <linasalekne@gmail.com>
2020-06-29 14:49:29 -0400

    added space bar func

--- script.js
+++ script.js
@@ -1,61 +1,75 @@
+
+// REMAKE THIS FROM SCRATCH WITH CALLS TO GET THE WORDS 
+// TIMER CALCULATOR AND STUFF
+// remember one step at a time - > the css and html styling can come last to an extent 
+
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
 
-quoteInputElement.addEventListener('input', () => {
-  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-  const arrayValue = quoteInputElement.value.split('')
+//const words = require('data.json');
+//console.log(words[1]);
 
-  let correct = true
-  arrayQuote.forEach((characterSpan, index) => {
-    const character = arrayValue[index]
-    if (character == null) {
-      characterSpan.classList.remove('correct')
-      characterSpan.classList.remove('incorrect')
-      correct = false
-    } else if (character === characterSpan.innerText) {
-      characterSpan.classList.add('correct')
-      characterSpan.classList.remove('incorrect')
-    } else {
-      characterSpan.classList.remove('correct')
-      characterSpan.classList.add('incorrect')
-      correct = false
-    }
-  })
 
-  if (correct) renderNewQuote()
-})
+var correct = 0;
+var incorrect = 0; 
+var currentQuote = '';
 
 function getRandomQuote() {
-  return fetch(RANDOM_QUOTE_API_URL)
+  const data = fetch(RANDOM_QUOTE_API_URL)
     .then(response => response.json())
     .then(data => data.content)
+  //currentQuote = data.toString().split(' ');
+  return data;
+
 }
 
 async function renderNewQuote() {
   const quote = await getRandomQuote()
+  //currentQuote = quote.split(' '); 
+  //console.log(currentQuote)
   quoteDisplayElement.innerHTML = ''
+  
   quote.split('').forEach(character => {
     const characterSpan = document.createElement('span')
     characterSpan.innerText = character
     quoteDisplayElement.appendChild(characterSpan)
   })
-  quoteInputElement.value = null
-  startTimer()
+  quoteInputElement.value = null;
 }
 
-let startTime
+
+renderNewQuote()
+
+
+quoteInputElement.addEventListener('keydown', e => {
+  if (e.key == ' ') {
+    if (quoteInputElement.value != null){
+      console.log(quoteInputElement.value);
+      if (quoteInputElement.value == currentQuote[1]){
+        console.log("a start");
+      }
+
+
+      quoteInputElement.value = null;
+
+    }
+  }
+
+})
+
+
+/*( let startTime
 function startTimer() {
   timerElement.innerText = 0
   startTime = new Date()
   setInterval(() => {
     timer.innerText = getTimerTime()
   }, 1000)
-}
+} */
 
 function getTimerTime() {
   return Math.floor((new Date() - startTime) / 1000)
 }
 
-renderNewQuote()
