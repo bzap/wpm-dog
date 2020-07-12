8c4c4630973b200c6b401ae575fd1b0115de5c34
linas <linasalekne@gmail.com>
2020-07-11 20:50:55 -0400

    big updates to sentence and struct

--- make_array.py
+++ make_array.py
@@ -1,9 +1,15 @@
 def make_array(): 
     out = []
-    file_object  = open("words.txt", "r") 
+    file_object  = open("wordsv2.txt", "r", encoding="utf8") 
+    
     #print(file_object.read())
     for line in file_object:
-        out.append(line[:-1])
+        word = []
+        string = ""
+        out.append(line.partition(' ')[0].replace('\n', ''))
+       # string = i.split(" ", 1)[0]
+    # out.append(string)
     print(out)
     print(len(out))
+
 make_array()
