c026501c5cbb84db20af4c3b09f28509798ac586
Linas Aleknevicius <aleknevl@mcmaster.ca>
2020-07-19 15:59:13 -0400

    fix math

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const DISP_ELEM = document.getElementById('display')
 const INP_ELEM = document.getElementById('input')
 const TIME_ELEM = document.getElementById('clock')
 const WPM_ELEM = document.getElementById('wpm')
+const WPM_NUM_ELEM = document.getElementById('wpmNum')
 const CASE_ELEM = document.getElementById('slider')
 const CORR_ELEM = document.getElementById('correctChar')
 const INCORR_ELEM = document.getElementById('incorrectChar')
 const GWPM_ELEM = document.getElementById('gwpm')
 const ACC_ELEM = document.getElementById('acc')
 const LIVE_WPM_ELEM = document.getElementById('liveWpm')
+
 
 var words = ['the', 'of', 'and', 'to', 'a', 'in', 'for', 'is', 'on', 'that', 'by', 'this', 'with', 'you', 'it', 'not', 'or', 'be', 'are', 'from', 'at', 'as', 'your', 'all', 'have', 'new', 'more', 'an', 'was', 'we', 'will', 'home', 'can', 'us', 'about', 'if', 'page', 'my', 'has', 'search', 'free', 'but', 'our', 'one', 'other', 'do', 'no', 'time', 'they', 'site', 'he', 'up', 'may', 'what', 'which', 'their', 'news', 'out', 'use', 'any', 'there', 'see', 'only', 'so', 'his', 'when', 'contact', 'here', 'business', 'who', 'web', 'also', 'now', 'help', 'get', 'view', 'online', 'first', 'am', 'been', 'would', 'how', 'were', 'me', 'services', 'some', 'these', 'click', 'its', 'like', 'service', 'than', 'find', 'price', 'date', 'back', 'top', 'people', 'had', 'list', 'name', 'just', 'over', 'state', 'year', 'day', 'into', 'email', 'two', 'health', 'world', 'next', 'used', 'go', 'work', 'last', 'most', 'products', 'music', 'buy', 'data', 'make', 'them', 'should', 'product', 'system', 'post', 'her', 'city', 'add', 'policy', 'number', 'such', 'please', 'available', 'copyright', 'support', 'message', 'after', 'best', 'software', 'then', 'good', 'video', 'well', 'where', 'info', 'rights', 'public', 'books', 'high', 'school', 'through', 'each', 'links', 'she', 'review', 'years', 'order', 'very', 'privacy', 'book', 'items', 'company', 'read', 'group', 'need', 'many', 'user', 'said', 'de', 'does', 'set', 'under', 'general', 'research', 'university', 'january', 'mail', 'full', 'map', 'reviews', 'program', 'life', 'know', 'games', 'way', 'days', 'management', 'part', 'could', 'great', 'united', 'hotel', 'real', 'item', 'international', 'center', 'ebay', 'must', 'store', 'travel', 'comments', 'made', 'development', 'report', 'off', 'member', 'details', 'line', 'terms', 'before', 'hotels', 'did', 'send', 'right', 'type', 'because', 'local', 'those', 'using', 'results', 'office', 'education', 'national', 'car', 'design', 'take', 'posted', 'internet', 'address', 'community', 'within', 'states', 'area', 'want', 'phone', 'dvd', 'information', 'shipping', 'reserved', 'subject', 'between', 'forum', 'family', 'long', 'based', 'code', 'show', 'even', 'black', 'check', 'special', 'prices', 'website', 'index', 'being', 'women', 'much', 'sign', 'file', 'link', 'open', 'today', 'technology', 'south', 'case', 'project', 'same', 'pages', 'uk', 'version', 'section', 'own', 'found', 'sports', 'house', 'related', 'security', 'both', 'county', 'american', 'photo', 'game', 'members', 'power', 'while', 'care', 'network', 'down', 'computer', 'systems', 'three', 'total', 'place', 'end', 'following', 'download', 'him', 'without', 'per', 'access', 'think', 'north', 'resources', 'current', 'posts', 'big', 'media', 'law', 'control', 'water', 'history', 'pictures', 'size', 'art', 'personal', 'since', 'including', 'guide', 'shop', 'directory', 'board', 'location', 'change', 'white', 'text', 'small', 'rating', 'rate', 'government', 'children', 'during', 'usa', 'return', 'students', 'shopping', 'account', 'times', 'sites', 'level', 'digital', 'profile', 'previous', 'form', 'events', 'love', 'old', 'john', 'main', 'call', 'hours', 'image', 'department', 'title', 'description', 'insurance', 'another', 'why', 'shall', 'property', 'class', 'cd', 'still', 'money', 'quality', 'every', 'listing', 'content', 'country', 'private', 'little', 'visit', 'save', 'tools', 'low', 'reply', 'customer', 'december', 'compare', 'movies', 'include', 'college', 'value', 'article', 'york', 'man', 'card', 'jobs', 'provide', 'food', 'source', 'author', 'different', 'press', 'learn', 'sale', 'around', 'print', 'course', 'job', 'canada', 'process', 'teen', 'room', 'stock', 'training', 'too', 'credit', 'point', 'join', 'science', 'men', 'categories', 'advanced', 'west', 'sales', 'look', 'english', 'left', 'team', 'estate', 'box', 'conditions', 'select', 'windows', 'photos', 'thread', 'week', 'category', 'note', 'live', 'large', 'gallery', 'table', 'register', 'however', 'june', 'october', 'november', 'market', 'library', 'really', 'action', 'start', 'series', 'model', 'features', 'air', 'industry', 'plan', 'human', 'provided', 'tv', 'yes', 'required', 'second', 'hot', 'accessories', 'cost', 'movie', 'forums', 'march', 'september', 'better', 'say', 'questions', 'july', 'yahoo', 'going', 'medical', 'test', 'friend', 'come', 'server', 'study', 'application', 'cart', 'staff', 'articles', 'san', 'feedback', 'again', 'play', 'looking', 'issues', 'april', 'never', 'users', 'complete', 'street', 'topic', 'comment', 'financial', 'things', 'working', 'against', 'standard', 'tax', 'person', 'below', 'mobile', 'less', 'got', 'blog', 'party', 'payment', 'equipment', 'login', 'student', 'let', 'programs', 'offers', 'legal', 'above', 'recent', 'park', 'stores', 'side', 'act', 'problem', 'red', 'give', 'memory', 'performance', 'social', 'august', 'quote', 'language', 'story', 'sell', 'options', 'experience', 'rates', 'create', 'key', 'body', 'young', 'important', 'field', 
 'few', 'east', 'paper', 'single', 'age', 'activities', 'club', 'example', 'additional', 'password', 'latest', 'something', 'road', 'gift', 'question', 'changes', 'night', 'hard', 'texas', 'oct', 'pay', 'four', 'poker', 'status', 'browse', 'issue', 'range', 'building', 'seller', 'court', 'february', 'always', 'result', 'audio', 'light', 'write', 'war', 'nov', 'offer', 'blue', 'groups', 'al', 'easy', 'given', 'files', 'event', 'release', 'analysis', 'request', 'fax', 'making', 'picture', 'needs', 'possible', 'might', 'professional', 'yet', 'month', 'major', 'star', 'areas', 'future', 'space', 'committee', 'hand', 'sun', 'cards', 'problems', 'london', 'meeting', 'rss', 'become', 'interest', 'id', 'child', 'keep', 'enter', 'share', 'similar', 'garden', 'schools', 'million', 'added', 'reference', 'companies', 'listed', 'baby', 
@@ -69,7 +71,9 @@ var flag;
 
 
 TIME_ELEM.innerHTML = 10 + 's';
-WPM_ELEM.innerHTML = '-- ' + 'WPM  ';
+WPM_ELEM.innerHTML = 'WPM  ';
+
+WPM_NUM_ELEM.innerHTML = '--';
 GWPM_ELEM.innerHTML = '00 ' + 'gWPM';
 ACC_ELEM.innerHTML = '00.0% ' + 'acc';
 
@@ -256,7 +260,8 @@ function renderResults() {
   }
   acc = (good / (good + bad)) * 100;
   
-  WPM_ELEM.innerHTML = (wpm + whitespaceCount).toFixed(0) +  ' WPM';
+  WPM_NUM_ELEM.innerHTML = (wpm + whitespaceCount).toFixed(0);
+  
   GWPM_ELEM.innerHTML = (gwpm + whitespaceCount).toFixed(0) + ' gWPM';
   ACC_ELEM.innerHTML = acc.toFixed(1) + '% acc';
   CORR_ELEM.innerHTML = 'correct chars: ' + (good + whitespaceCount);
@@ -476,7 +481,8 @@ function reset(){
   INP_ELEM.disabled = false;
   INP_ELEM.value = '';
   DISP_ELEM.innerHTML = '';
-  WPM_ELEM.innerHTML = '102 ' + 'WPM  ';
+  WPM_NUM_ELEM.innerHTML = '--';
+  WPM_ELEM.innerHTML = 'WPM  ';
   GWPM_ELEM.innerHTML = '00 ' + 'gWPM';
   ACC_ELEM.innerHTML = '00.0% acc';
   CORR_ELEM.innerHTML = 'correct chars: 00';
